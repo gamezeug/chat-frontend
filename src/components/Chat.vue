518037d73058e8ec86a42a9f21a4aa693e26a0d3
Artun Subasi <asubasi@gmail.com>
2019-02-17 17:11:03 +0100

    Just a bordered div with text

---
+++ src/components/Chat.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="home" class="container-fluid border rounded">
+    <div class="row">
+        <div class="col chat-column">
+        {{msg}}
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#home {
+  min-height: 300px;
+  height: 100%;
+}
+</style>
