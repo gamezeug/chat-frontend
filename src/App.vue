d9acb499fd314d57e5392e80e50ae00afcd25cc9
Artun Subasi <asubasi@gmail.com>
2019-02-17 18:17:54 +0100

    Some simple vue stuff...

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Chat msg="Welcome to Your Vue.js App"/>
+    <Chat/>
   </div>
 </template>
 
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="home" class="container-fluid border rounded">
     <div class="row">
-        <div class="col chat-column">
-        {{msg}}
+        <div class="col messages">
+          <ul class="border border-primary rounded">
+            <li v-for="m in messages" :key="m">
+              {{ m }}
+            </li>
+          </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+          <form class="form-inline">
+            <div class="form-group mr-2">
+              <input type="text" class="form-control border border-primary" v-model="input" placeholder="Send a message"/>
+            </div>
+            <button type="button" class="btn btn-primary" v-on:click="sendMessage">Send</button>
+          </form>
         </div>
     </div>
   </div>
@@ -11,16 +25,27 @@
 <script>
 export default {
   name: 'Chat',
-  props: {
-    msg: String
+  data: function () {
+    return {
+      input: '',
+      messages: []
+    }
+  },
+  methods: {
+    sendMessage: function () {
+      this.$data.messages.push(this.$data.input)
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#home {
-  min-height: 300px;
-  height: 100%;
+.messages {
+  min-height: 100px;
+  height: 300px;
+}
+ul {
+  height: 90%;
+  overflow: auto;
 }
 </style>
